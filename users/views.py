a4a7cec23d87bb7eba3917cf312a269a00871966
Dominik Halle <dominik.halle@schnapptack.de>
2017-12-21 15:20:27 +0100

    removed deprecation warning for django20

--- users/views.py
+++ users/views.py
@@ -1,11 +1,10 @@
-# coding=utf-8
-from __future__ import unicode_literals, print_function
-import functools
-import warnings
+from __future__ import unicode_literals
+
 # Avoid shadowing the login() and logout() views below.
 from django.contrib.auth import (
     get_user_model
 )
+from django.contrib.auth import login
 from django.contrib.auth.forms import (
     SetPasswordForm,
 )
@@ -14,40 +13,16 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import resolve_url
 from django.template.response import TemplateResponse
 from django.urls import reverse
-from django.utils.deprecation import RemovedInDjango20Warning
 from django.utils.encoding import force_text
 from django.utils.http import urlsafe_base64_decode
 from django.utils.translation import ugettext as _
 from django.views.decorators.cache import never_cache
 from django.views.decorators.debug import sensitive_post_parameters
-from django.contrib.auth import login
-
-
-def deprecate_current_app(func):
-    """
-    Handle deprecation of the current_app parameter of the views.
-    """
-    @functools.wraps(func)
-    def inner(*args, **kwargs):
-        if 'current_app' in kwargs:
-            warnings.warn(
-                "Passing `current_app` as a keyword argument is deprecated. "
-                "Instead the caller of `{0}` should set "
-                "`request.current_app`.".format(func.__name__),
-                RemovedInDjango20Warning
-            )
-            current_app = kwargs.pop('current_app')
-            request = kwargs.get('request', None)
-            if request and current_app is not None:
-                request.current_app = current_app
-        return func(*args, **kwargs)
-    return inner
 
 
 # Doesn't need csrf_protect since no-one can guess the URL
 @sensitive_post_parameters()
 @never_cache
-@deprecate_current_app
 def password_reset_confirm(request, uidb64=None, token=None,
                            template_name='registration/password_reset_confirm.html',
                            token_generator=default_token_generator,
